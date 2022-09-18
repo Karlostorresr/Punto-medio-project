0bb277867c32bd5d18cf87cb5f1815a5e1274792
FernandoAG <fernando.a.aguilarcano@gmail.com>
2022-09-18 16:18:52 -0500

    Se agrega pagina de contacto

---
+++ JS/ScriptFinale.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict'
+  
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+
+
+//"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
